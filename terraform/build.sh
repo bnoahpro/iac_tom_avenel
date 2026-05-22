11194b0ff23004bb5c5de36c8faec7ce722d0797
bnoahpro <noahbaptista.pro@gmail.com>
2026-05-22 17:22:01 +0200

    add argocd

--- terraform/build.sh
+++ terraform/build.sh
@@ -7,7 +7,7 @@ echo "==> validate..."
 terraform validate .
 
 echo "==> Apply..."
-terraform apply -auto-approve -var-file=./common.tfvars -var-file=build.tfvars
+terraform apply -auto-approve -var-file=common.tfvars -var-file=build.tfvars
 
 echo ""
 echo "==> Déployé !"
